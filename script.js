b8111f13f72d0b3807b6df8e92c3b649a4947fa0
Helen <180019665+helenbun@users.noreply.github.com>
2024-09-07 15:45:33 +0100

    Add function to get human choice
    getHumanChoice prompts user for a choice, checks if it is valid uaing a
    while loop. If not valid, prompts again, otherwise returns human choice.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Get computer guess by generating a random number between 1 and 99. 
-function computerChoice() {
+function getComputerChoice() {
     let choiceNumber = 0;
     while (choiceNumber === 0) {
         choiceNumber = Math.floor(Math.random() * 100);
@@ -15,4 +15,18 @@ function computerChoice() {
     }   
 }
 
-console.log(computerChoice());
+//Take user input, if it is not rock, paper or scissors, prompt again. If valid option, return it.
+
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, paper or scissors?");
+    humanChoice = humanChoice.toLowerCase();
+    while (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
+        humanChoice = prompt("Invalid choice, please input rock, paper or scissors.");
+        humanChoice = humanChoice.toLowerCase();
+    }
+    return humanChoice;
+
+}
+
+console.log(getComputerChoice());
+console.log(getHumanChoice());
