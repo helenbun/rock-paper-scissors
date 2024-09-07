4bcb3268813601e58d1a2edd572053e03a147d27
Helen <180019665+helenbun@users.noreply.github.com>
2024-09-07 16:28:31 +0100

    Add playRound function
    The playRound function gets the human and computer choices, compares
    them and declares the winner, incrementing the winner's score.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 //Get computer guess by generating a random number between 1 and 99. 
 function getComputerChoice() {
     let choiceNumber = 0;
@@ -28,5 +31,61 @@ function getHumanChoice() {
 
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+//For one round, get human choice and computer choice.
+//Determine the winner of the round.
+//Print the winner to the console.
+//Increment the human or computer score.
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    let winner = null;
+    if (computerChoice === humanChoice) {
+        console.log(`You both chose ${computerChoice}. It's a draw!`);
+    }
+    else {
+        switch (humanChoice){
+            case "rock":
+                if (computerChoice === "paper") {
+                    console.log("Paper covers rock, you lose!")
+                    winner = "computer";
+                    break;
+                }
+                else {
+                    console.log("Rock breaks scissors, you win!")
+                    winner = "human";
+                    break;
+                }
+            case "scissors":
+                if (computerChoice === "paper") {
+                    console.log("Scissors cut paper, you win!")
+                    winner = "human";
+                    break;
+                }
+                else {
+                    console.log("Rock breaks scissors, you lose!")
+                    winner = "computer";
+                    break;
+                }
+            case "paper":
+                if (computerChoice === "scissors") {
+                    console.log("Scissors cut paper, you lose!")
+                    winner = "computer";
+                    break;
+                }
+                else {
+                    console.log("Paper covers rock, you win!")
+                    winner = "human";
+                    break;
+                }
+        }
+    if (winner === "human"){
+        humanScore += 1;
+    }
+    else {
+        computerScore += 1;
+        }
+    }
+}
+
+playRound();
